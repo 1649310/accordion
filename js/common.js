c1566aaf3f2ce53f78431822c8258b4b6e3b41e5
Yuliya <mikhaylenkoyuliya@gmail.com>
2019-02-28 13:24:27 +0200

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,6 +1,6 @@
-$(function() {
-	$(".burger").on("click", function() {
-	  $(this).toggleClass("open");
-	  $(".header__menu  .nav").toggleClass("open");
+jQuery(function($){
+	$('.accordion__item-head').on('click', function(e){ 
+		$('.accordion__item').removeClass('open'); 
+		$(this).parent('.accordion__item').toggleClass('open');
 	});
-  });
+})
